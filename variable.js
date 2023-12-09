e1260abf249c6e8949746fafa7ef15c1a3c331ce
FH Himel <fhhimel17@gmail.com>
2023-12-10 01:54:57 +0600

    added-00

--- variable.js
+++ variable.js
@@ -11,5 +11,19 @@ console.log(y);
 const z = 45;
 console.log(z);
 
-// 
+// numeric
+var age = 23;
+var temperature = 30;
+var price = 150;
+
+// String
+var name = 'Himel';
+var location = 'bangladesh';
+var gender = 'male';
+
+// Boolean
+var unmarried = true;
+var male = true;
+var brilliant = false;
+
 
