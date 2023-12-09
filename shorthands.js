ea9b67aeafa0b522fbff4c4057967a778c102a12
FH Himel <fhhimel17@gmail.com>
2023-12-10 02:25:59 +0600

    added=005

---
+++ shorthands.js
@@ -0,0 +1,18 @@
+// shorthands
+var hey = 502;
+var hii = 50;
+var total = hey + hii ;
+console.log(total);
+
+total += 14;
+console.log(total);
+
+total -= 14;
+console.log(total);
+
+total *= 14;
+console.log(total);
+
+total /= 14;
+console.log(total);
+
