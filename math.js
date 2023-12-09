c1a4e8fddd982b2dc0b9af3ca14efdd30305be24
FH Himel <fhhimel17@gmail.com>
2023-12-10 01:49:39 +0600

    added

---
+++ math.js
@@ -0,0 +1,16 @@
+ /*************
+ *** 1.number ==> 55 , 10.50 , -65
+ *** 2.integer , float , odd , even
+ *** 3.float: decimal : 2.5 , 3.6 , 9.6 
+ *** 4.operation : addition , substraction , multiplication , division, remainder
+ *** 5.percent
+ *** 6.average
+ *** 7.comparison: < ; > ; <= ; >= ; == ; !==
+ *** 8.oikik / unitary
+ *** 9. max , min , average , sum
+ *** 10. series
+ *:::Extar::::::
+ **Prime
+ **Permutation
+ **Area
+ */
---
+++ variable.js
@@ -0,0 +1,15 @@
+// vary + able = variable
+
+var x = 15;
+x = 100;
+console.log(x);
+
+let y = 30;
+y = 150;
+console.log(y);
+
+const z = 45;
+console.log(z);
+
+// 
+
