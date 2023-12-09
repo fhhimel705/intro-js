962b88c5a275d5ff99dbc2589158d3cf5e8ebb0a
FH Himel <fhhimel17@gmail.com>
2023-12-10 02:49:31 +0600

    added-008

--- type.js
+++ type.js
@@ -8,3 +8,23 @@ console.log(price_02);
 var affordable = true;
 console.log(typeof affordable);
 
+// sometimes the result shows too many digits in float number
+var x = 0.1;
+var y = 0.2;
+var sum = x+y;
+console.log(sum);
+
+// you can fix it like this but the problem is this will return string as a result
+sum = sum.toFixed(2);
+console.log(sum);
+console.log(typeof sum);
+
+// To fix it you have convert it into float
+sum = parseFloat(sum);
+console.log(typeof sum);
+
+
+
+
+
+
