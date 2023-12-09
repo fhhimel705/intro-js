da2348c1ddaa21f9e5df4212a1bf641ee9935fb7
FH Himel <fhhimel17@gmail.com>
2023-12-10 02:40:45 +0600

    added-07

---
+++ type.js
@@ -0,0 +1,10 @@
+var price = 100;
+console.log(typeof price);
+
+var price_01 = '100';
+var price_02 = typeof(price_01);
+console.log(price_02);
+
+var affordable = true;
+console.log(typeof affordable);
+
