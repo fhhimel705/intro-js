19e0d99c6529c59344372419eadb354c76eb1c21
FH Himel <fhhimel17@gmail.com>
2023-12-10 02:21:54 +0600

    added-004

---
+++ operation.js
@@ -0,0 +1,32 @@
+// operations  = add , substraction , division , multiplication
+// add
+
+var orange = 50;
+var mango = 100;
+var total = orange + mango;
+console.log(total);
+
+// substraction
+
+var banana = 50;
+var grap = 100;
+var total_02 = banana - grap;
+console.log(total_02);
+
+// multiplication
+var orange_02 = 50;
+var mango_02 = 100;
+var total_03 = orange_02 * mango_02;
+console.log(total_03);
+
+// division
+var orange_03 = 50;
+var mango_03 = 100;
+var total_04 = orange_03 / mango_03;
+console.log(total_04);
+
+// remainder
+var orange_04 = 19;
+var mango_04 = 3;
+var total_05 = orange_04 % mango_04;
+console.log(total_05);
