122e2034703f6619477e8b2141fe578336568612
FH Himel <fhhimel17@gmail.com>
2023-12-10 02:10:23 +0600

    added-001

---
+++ variable-name.js
@@ -0,0 +1,29 @@
+// good variable
+var age = 50;
+
+//variable name cant be any keywords
+var false = 585;
+var return = true;
+
+// variable name cant be more than one word
+var hi heelo hey = 'not okay' ;
+
+//  variable name cant have  qutoes
+var 'name' = 'not okay';
+
+// var names cant start with numbers but can end 
+var 564hello = 'not okay' ;
+var hey47 = 'okay';
+
+// how to use long names
+// cant use dash '-'
+var hello-bro = 'not okay';
+var hello_bro = 'okay';
+var hellobro = 'not suitable';
+var hello bro = 'not okay';
+var helloBro = 'camel Case'
+var whatsUp = 'thats more like it';
+
+// var name  is  case sensitive
+var people = 50;
+var People = 40;
