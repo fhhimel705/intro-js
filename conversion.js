e64116f076a554ddd94f4fc56cfda6d44d65a060
FH Himel <fhhimel17@gmail.com>
2023-12-10 02:34:35 +0600

    added-006

---
+++ conversion.js
@@ -0,0 +1,17 @@
+var x =50;
+var y = '50';
+var total = x + y;
+console.log(total);
+
+var x_01 = parseInt(x);
+console.log(x_01);
+
+var gpa = 3.55;
+var gpa_01 = parseInt(gpa);
+console.log(gpa_01);
+
+var gpa_02 = parseFloat(gpa);
+console.log(gpa_02);
+
+
+
